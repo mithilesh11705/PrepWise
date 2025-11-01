14264968bb2a0d29d9c8f8973b850506debd9175
Mithilesh Deore <mithileshdeore7@gmail.com>
2025-11-01 12:44:39 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,11 +2,12 @@
 
 import { GoogleGenerativeAI } from '@google/generative-ai';
 
-const genAI = new GoogleGenerativeAI(import.meta.env?.VITE_GEMINI_API_KEY ) ;
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const genAI = new GoogleGenerativeAI(import.meta.env?.VITE_GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 export const chatSession = model.startChat({
   generationConfig: {
     responseMimeType: 'application/json',
   },
 });
+
 // console. log(response. text()); 
